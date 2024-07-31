ec262ee42ec893a614f7b7b054c6b707c5766eb1
Nicolas <nicolas.salgado@ae.studio>
2024-07-31 18:04:28 -0300

    feat: create item with due time

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,7 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 module.exports = {
   root: true,
   'extends': [
+    "plugin:tailwindcss/recommended",
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
--- src/components/TodoComponents/TodoCard.vue
+++ src/components/TodoComponents/TodoCard.vue
@@ -1,12 +1,13 @@
 <template>
   <div ref="editPopupTarget" class="relative">
-    <BaseCard class="bg-white p-4 flex justify-between items-center border hover:border-border gap-8" @mouseenter="isHovering = true"
+    <BaseCard class="flex items-center justify-between gap-8 border bg-white px-4 pb-4 pt-6 hover:border-border" @mouseenter="isHovering = true"
       @mouseleave="isHovering = false">
-      <input class="rounded-md border px-2 py-2 flex-1 focus:outline-none" v-if="isEditing" v-model="newValue" />
-      <span v-else class="overflow-ellipsis whitespace-nowrap overflow-hidden">
+      <input class="flex-1 rounded-md border p-2 focus:outline-none" v-if="isEditing" v-model="newValue" />
+      <span v-else class="truncate">
         {{ todoItem.title }}
       </span>
-      <PencilSquareIcon v-if="isHovering && !isEditing" class="size-4 text-zinc-900 shrink-0 hover:cursor-pointer hover:text-zinc-600"
+      <span class="absolute right-4 top-2.5 text-xs font-thin text-secondary-200">{{ humanizedDuration }}</span>
+      <PencilSquareIcon v-if="isHovering && !isEditing" class="size-4 shrink-0 text-zinc-900 hover:cursor-pointer hover:text-zinc-600"
         @click="isEditing = true" />
     </BaseCard>
     <!-- <EditPopup v-if="isEditing" @save="onSaveChanges" /> -->
@@ -27,7 +28,7 @@ import database from '@/providers/database';
 import { PencilSquareIcon } from '@heroicons/vue/24/outline';
 import { onClickOutside } from '@vueuse/core';
 import type { PropType } from 'vue';
-import { ref, toRefs } from 'vue';
+import { computed, ref, toRefs } from 'vue';
 import BaseCard from '../BaseCard.vue';
 
 const db = database()
@@ -38,6 +39,11 @@ const isHovering = ref(false)
 const isEditing = ref(false)
 const newValue = ref(todoItem.value.title)
 const editPopupTarget = ref(null)
+const humanizedDuration = computed(() => {
+  const hours = Math.floor(todoItem.value.dueTime / 60) || 0
+  const minutes = todoItem.value.dueTime % 60 || 0
+  return `${hours}h ${minutes}m`
+})
 
 onClickOutside(editPopupTarget, () => isEditing.value = false)
 
